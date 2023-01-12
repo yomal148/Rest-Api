b0589eb8421cd813581f60b1ab4cf893909d13f2
Amal Tharyan <amaltharyan@Amals-MacBook-Air.local>
2023-01-12 18:56:33 -0500

    adding to basic.py

---
+++ py_client/basic.py
@@ -0,0 +1,11 @@
+import requests
+
+#urls
+#declaring endpoints, use an actual API endpoint
+endpoint = "https://httpbin.org/"
+endpoint = "https://httpbin.org/status/200/"
+
+requests.get() # Application programming interface
+
+# Not REST APIs, these are library apis
+# Phone -> Camera -> App -> API -> Camera
